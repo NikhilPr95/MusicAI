75acd6f44f80106e7dc03f71ccb01e22a129c8e6
NikhilPr95 <nikhill.prabhu@gmail.com>
2018-02-14 22:39:21 +0530

    Calls python if python3 absent in batch_format

--- musicai/main/preprocessing/batch_format.py
+++ musicai/main/preprocessing/batch_format.py
@@ -5,5 +5,5 @@ try:
     list(map(lambda song: os.system("python3 chord_creation.py " + song),
              glob.glob("../../data/processed/*.csv.formatted")))
 except:
-    list(map(lambda song: os.system("python3 chord_creation.py " + song),
+    list(map(lambda song: os.system("python chord_creation.py " + song),
              glob.glob("../../data/processed/*.csv.formatted")))
