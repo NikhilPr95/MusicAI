27e3d391d574187f4bf34a59199c3765c37c7892
Chittaranjan <vrchittaranjan@gmail.com>
2018-03-27 11:56:38 +0530

    tweaks note duration

--- musicai/main/generation/client.py
+++ musicai/main/generation/client.py
@@ -3,8 +3,10 @@ from musicai.tests.split import *
 from musicai.main.lib.input_vectors import sequence_vectors, parse_data
 from musicai.main.constants.directories import *
 import glob
+import os.path
 def gen_trigram():
 	train, val, test = splitData()
+	#[os.path.join(PROCESSED_CHORDS_TIME,"baba.csv.formatted")]
 	bar_sequences,chord_sequences = parse_data(train, octave=True,  generated=True)
 	print(len(flatten(bar_sequences)),len(flatten(bar_sequences)))
 	X_train = flatten(flatten(bar_sequences))
@@ -12,7 +14,7 @@ def gen_trigram():
 	ngrams = list(zip(flatten(bar_sequences),flatten(chord_sequences)))
 	ngrams = [(gram[0][i],gram[1]) for gram in ngrams for i in range(len(gram[0])) ]
 	obj = Text(ngrams)
-	trigrams = obj.generate_trigram_sequences(30)
+	trigrams = obj.generate_trigram_sequences(300)
 	chords = trigrams[0][0:2] + tuple(i[2] for i in trigrams[1:])
 	return chords
 
--- musicai/main/scheduler/generator.py
+++ musicai/main/scheduler/generator.py
@@ -9,20 +9,29 @@ import SharedArray as sa
 from musicai.main.generation.client import *
 from musicai.utils.chords import get_notes, get_chord_mapping
 from musicai.main.lib.input_vectors import reduce
-
+import random
 
 def sendOutput():
 	# puts the output to the keyboard, gives the midi output back after reading from shared memory
 	# poll shared mem
 	# notesToPlay = [[48,100],[48,100],[48,100],[48,100]]
-	for i in range(len(leftHandNotes)):
-		out.note_on(60 + int(rightHandNotes[i][0][0]), velocity=rightHandNotes[i][1])
-		out.note_on(48 + int(rightHandNotes[i]), velocity=rightHandNotes[i][1])
+	print(leftHandNotes, rightHandNotes)
+	
+	for i in range(len(rightHandNotes)):
+		if i%4 == 0:
+			for l in range(48,60):
+				out.note_off(l, velocity=0)
+			for note in leftHandNotes[i]:
+				out.note_on(48 + int(note), velocity=rightHandNotes[i][1])	
+		if (rightHandNotes[i][2] > 200 and rightHandNotes[i][2] < 1000):
+			out.note_on(60 + int(rightHandNotes[i][0]), velocity=rightHandNotes[i][1])
+		#out.note_on(48 + int(leftHandNotes[i]), velocity=rightHandNotes[i][1])
 		# time.sleep((60 / tempo))
-		time.sleep(rightHandNotes[i][0][2])
-		print("OUT")
-		out.note_off(60 + int(rightHandNotes[i][0][0]), velocity=0)
-		out.note_off(48 + int(leftHandNotes[i]), velocity=0)
+			time.sleep(rightHandNotes[i][2]/1000)
+		#print("OUT")
+			out.note_off(60 + int(rightHandNotes[i][0]), velocity=0)
+		#out.note_off(48 + int(leftHandNotes[i]), velocity=0)
+
 
 if __name__ == "__main__":
 	tempo = 80  # define
@@ -33,7 +42,7 @@ if __name__ == "__main__":
 	rightHandNotes = [i[0] for i in ngramsGenerated]  # generating notes only.
 	print("Right hand notes ",rightHandNotes)
 	leftHandNotes = [(get_notes(ngram[1],chordMaps)) for ngram in ngramsGenerated]
-	leftHandNotes = flatten(leftHandNotes)
+	#leftHandNotes = flatten(leftHandNotes)
 	print("Left hand notes : ", leftHandNotes)
 	out = pygame.midi.Output(2)
 	sendOutput()
